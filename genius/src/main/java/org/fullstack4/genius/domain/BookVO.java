1cca9a1c9e4b399f4eb4a74eeb06bcd14299ab7c
joyun <oto11914@naver.com>
2024-04-25 19:51:22 +0900

    etc: book mapper, sevice test 추가

--- genius/src/main/java/org/fullstack4/genius/domain/BookVO.java
+++ genius/src/main/java/org/fullstack4/genius/domain/BookVO.java
@@ -1,15 +1,17 @@
 package org.fullstack4.genius.domain;
 
 import jdk.vm.ci.meta.Local;
+import lombok.*;
+import lombok.extern.log4j.Log4j2;
 
 import java.time.LocalDate;
 
-//@Log4j2///실제 배포시에 넣지마
-//@ToString
-//@Getter
-//@AllArgsConstructor
-////@NoArgsConstructor
-//@Builder
+@Log4j2///실제 배포시에 넣지마
+@ToString
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
 public class BookVO {
     private int book_idx;
     private String book_code;
--- genius/src/main/java/org/fullstack4/genius/service/BookServiceImpl.java
+++ genius/src/main/java/org/fullstack4/genius/service/BookServiceImpl.java
@@ -6,23 +6,39 @@ import org.fullstack4.genius.domain.BookVO;
 import org.fullstack4.genius.dto.BookDTO;
 import org.fullstack4.genius.dto.PageRequestDTO;
 import org.fullstack4.genius.mapper.BookMapper;
+import org.modelmapper.ModelMapper;
 import org.springframework.stereotype.Service;
 
+import java.awt.print.Book;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Log4j2
 @Service
 @RequiredArgsConstructor
 public class BookServiceImpl implements BookServiceIf {
+    private final BookMapper bookMapper;
+    private final ModelMapper modelMapper;
+
 
     @Override
-    public int regist(BookVO bbsVO) {
-        return 0;
+    public int regist(BookDTO bookDTO) {
+        BookVO bookVO = modelMapper.map(bookDTO, BookVO.class);
+        log.info("==========================");
+        log.info("BookServiceImpl >> regist(bookDTO) : " + bookDTO.toString());
+        int result = bookMapper.regist(bookVO);
+        log.info("BookServiceImpl >> regist(bookVO) : " + bookVO.toString());
+        log.info("BookServiceImpl >> result : " + result);
+        log.info("==========================");
+        return result;
     }
 
     @Override
     public List<BookDTO> listAll() {
-        return null;
+        List<BookDTO> bookDTOList = bookMapper.listAll().stream()
+                .map(bookVO -> modelMapper.map(bookVO,BookDTO.class))
+                .collect(Collectors.toList());
+        return bookDTOList;
     }
 
     @Override
@@ -37,7 +53,12 @@ public class BookServiceImpl implements BookServiceIf {
 
     @Override
     public int delete(int idx) {
-        return 0;
+        log.info("==========================");
+        log.info("BbsServiceImpl >> delete(int idx) : " + idx);
+        int result = bookMapper.delete(idx);
+        log.info("BbsServiceImpl >> result : " + result);
+        log.info("==========================");
+        return result;
     }
 
     @Override
