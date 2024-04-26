2d10716e83768ae4a28a5015aaa8b5251f93a3d4
joyun <oto11914@naver.com>
2024-04-26 21:11:18 +0900

    etc : admin/book 상품 리스트 출력

--- genius/src/main/java/org/fullstack4/genius/controller/AdminBookController.java
+++ genius/src/main/java/org/fullstack4/genius/controller/AdminBookController.java
@@ -3,25 +3,52 @@ package org.fullstack4.genius.controller;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
+import org.fullstack4.genius.dto.BookDTO;
+import org.fullstack4.genius.dto.PageRequestDTO;
+import org.fullstack4.genius.dto.PageResponseDTO;
+import org.fullstack4.genius.service.BookServiceIf;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+import javax.validation.Valid;
+import java.util.List;
+import java.util.Map;
 
 @Log4j2
 @Controller
 @RequestMapping(value="/admin/book")
 @RequiredArgsConstructor
 public class AdminBookController {
-
+    private final BookServiceIf bookServiceIf;
     @GetMapping("/payview")
     public void GETPayView(){
 
     }
 
     @GetMapping("/itemlist")
-    public void GETItemList(){
+    public void GETItemList(
+            @Valid PageRequestDTO pageRequestDTO,
+            BindingResult bindingResult,
+            RedirectAttributes redirectAttributes,
+            Model model){
+        if(bindingResult.hasErrors()){
+            log.info("BbsController >> list Error");
+            redirectAttributes.addFlashAttribute("errors", bindingResult.getAllErrors());
+        }
+        pageRequestDTO.setPage_size(9);
+        pageRequestDTO.setPage_block_size(10);
+        PageResponseDTO<BookDTO> responseDTO = bookServiceIf.BookListByPage(pageRequestDTO);
 
+//        List<Map<String,String>> subjectList = bookServiceIf.bookSubjectCategoryList();
+//        List<Map<String,String>> classList = bookServiceIf.bookClassCategoryList();
+//        model.addAttribute("subjectList",subjectList);
+//        model.addAttribute("classList",classList);
+        model.addAttribute("responseDTO", responseDTO);
     }
 
     @GetMapping("/itemRegist")
