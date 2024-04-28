df7448e78e72e1c8d54f6135e9cbb1c93a304c12
Geeehyun <wkdwl578@gmail.com>
2024-04-28 21:01:59 +0900

    feat : 탈퇴하기 기능 구현 완료!

--- genius/src/main/java/org/fullstack4/genius/service/MemberServiceIf.java
+++ genius/src/main/java/org/fullstack4/genius/service/MemberServiceIf.java
@@ -8,8 +8,7 @@ public interface MemberServiceIf {
 
     int join(MemberDTO memberDTO);
     MemberDTO view(String user_id);
-    int update(MemberDTO memberDTO);
-    int leave(String user_id);
+    int leave(String member_id);
     int idCheck(String user_id);
 
     MemberDTO login(String member_id, String pwd);
