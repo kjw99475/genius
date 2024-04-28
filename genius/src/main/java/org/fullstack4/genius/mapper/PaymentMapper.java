fa007cc47fa51ee120484cf3e1af56cc4ee35bd4
DESKTOP-7DN2GF6\pc <leehv1234@kookmin.ac.kr>
2024-04-29 02:56:13 +0900

    etc:내일할거 미리 작성

--- genius/src/main/java/org/fullstack4/genius/mapper/PaymentMapper.java
+++ genius/src/main/java/org/fullstack4/genius/mapper/PaymentMapper.java
@@ -8,12 +8,13 @@ import java.util.List;
 public interface PaymentMapper {
 
     int charge(PaymentVO PaymentVO);
+    int usepoint(PaymentVO PaymentVO);
     int memberPay(PaymentVO paymentVO);
     int totalCount(String member_id);
     List<PaymentVO> listAll(String user_id);
     PaymentVO view(String member_id);
     int pointview(String member_id);
-    int modify(PaymentVO Paymentvo);
+    int refund(PaymentVO Paymentvo);
     int delete(PaymentVO Paymentvo);
     int PaymentTotalCount(PageRequestDTO requestDTO);
     List<PaymentVO> PaymentListByPage(PageRequestDTO requestDTO);
