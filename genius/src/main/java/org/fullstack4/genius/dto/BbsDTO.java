7b965a6bb381825269b3edc0513d3afdc4baac51
DESKTOP-0H1N2HM\kjw <kjw99475@gmail.com>
2024-05-04 17:33:10 +0900

    etc: 유효성검사 백단 추가중

--- genius/src/main/java/org/fullstack4/genius/dto/BbsDTO.java
+++ genius/src/main/java/org/fullstack4/genius/dto/BbsDTO.java
@@ -8,6 +8,7 @@ import lombok.NoArgsConstructor;
 import lombok.extern.log4j.Log4j2;
 import org.apache.logging.log4j.core.config.plugins.validation.constraints.NotBlank;
 
+import javax.validation.constraints.PositiveOrZero;
 import java.time.LocalDate;
 
 @Log4j2
@@ -20,15 +21,14 @@ public class BbsDTO {
     @NotBlank
     private String bbs_title;
     private String category_code;
-    @NotBlank
     private String member_id;
     private String member_name;
-//    @Builder.Default
-//    private String fileYN="N";
     private String fileYN;
     @NotBlank
     private String bbs_contents;
     private LocalDate reg_date;
     private LocalDate modify_date;
-    private int read_cnt;
+    @PositiveOrZero
+    @Builder.Default
+    private int read_cnt=0;
 }
